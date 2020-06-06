03a2bfc8a7b38eaafa375db9334f3a0ef89775ef
BigPoppaG <daniel.gelderd@gmail.com>
2020-06-06 15:02:14 +0100

    Added Question component rendered on main template

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Question number=1 text="What is 37 - 28?"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Question from './components/Question.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Question
   }
 }
 </script>
---
+++ src/components/Question.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="question">
+    <div class="qestion-number">{{ number }}</div>  
+    <div class="question-text">{{ text }}</div>
+    <div class="question-answer"><input v-model="answer" ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: { 
+    number: Number, 
+    text: String, 
+  }, 
+  data: function() { 
+    return { 
+      answer: ''    
+    } 
+  }, 
+  computed: { 
+     
+  }, 
+  
+  watch: { 
+    answer: function (value, oldValue) { 
+      return value + oldValue
+     } 
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>

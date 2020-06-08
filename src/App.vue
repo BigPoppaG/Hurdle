8f6d9493bcd3c698f412cacff77df5834ee27b73
BigPoppaG <daniel.gelderd@gmail.com>
2020-06-08 22:58:49 +0100

    Added vuetify components and added vue router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <template>
 
   <div id="app">
+    <v-app>
+      <v-navigation-drawer app>
+        <!-- -->
+      </v-navigation-drawer>
+
+      <v-app-bar app>
+        <!-- -->
+      </v-app-bar>
+
+      <!-- Sizes your content based upon application components -->
+      <v-content>
+
+        <!-- Provides the application the proper gutter -->
+        <v-container fluid>
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link>
+        </v-container>
+      </v-content>
+
+      <v-footer app>
+        <!-- -->
+      </v-footer>
+    </v-app>
     <Question :question="questions[0]" />
     <br/>
     <QuestionEdit />
+
   </div>
 </template>
 
